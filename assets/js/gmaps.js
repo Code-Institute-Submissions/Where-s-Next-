5a791f52454097c9e4e76742573cfcae31fcd33e
Leeap83 <leeap83@gmail.com>
2021-01-07 14:52:45 +0000

    added  google map to html and added the funtion and options to gmaps.js

---
+++ assets/js/gmaps.js
@@ -0,0 +1,8 @@
+function initMap() {
+    var options = {
+        zoom: 4,
+        center: { lat: 53.29345, lng: -9.4764513},
+    }
+  
+  var map = new google.maps.Map(document.getElementById("map"), options);
+}
