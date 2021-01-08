2a586f80faecea61b89a9dd5f31a7b373701ff77
Leeap83 <leeap83@gmail.com>
2021-01-08 17:47:10 +0000

    JS started for search results for google maps and places

--- assets/js/gmaps.js
+++ assets/js/gmaps.js
@@ -1,8 +1,234 @@
+var map, places, infoWindow;
+var markers = [];
+var autocomplete;
+var countryRestrict = { country: [] };
+var MARKER_PATH =
+  "https://developers.google.com/maps/documentation/javascript/images/marker_green";
+
+var hostnameRegexp = new RegExp("^https?://.+?/");
+
+//List of Countries and properties
+var countries = {
+    au: {
+    center: { lat: -25.3, lng: 133.8 },
+    zoom: 4,
+  },
+  br: {
+    center: { lat: -14.2, lng: -51.9 },
+    zoom: 3,
+  },
+  ca: {
+    center: { lat: 62, lng: -110.0 },
+    zoom: 3,
+  },
+  fr: {
+    center: { lat: 46.2, lng: 2.2 },
+    zoom: 5,
+  },
+  de: {
+    center: { lat: 51.2, lng: 10.4 },
+    zoom: 5,
+  },
+  mx: {
+    center: { lat: 23.6, lng: -102.5 },
+    zoom: 4,
+  },
+  nz: {
+    center: { lat: -40.9, lng: 174.9 },
+    zoom: 5,
+  },
+  it: {
+    center: { lat: 41.9, lng: 12.6 },
+    zoom: 5,
+  },
+  za: {
+    center: { lat: -30.6, lng: 22.9 },
+    zoom: 5,
+  },
+  es: {
+    center: { lat: 40.5, lng: -3.7 },
+    zoom: 5,
+  },
+  pt: {
+    center: { lat: 39.4, lng: -8.2 },
+    zoom: 6,
+  },
+  us: {
+    center: { lat: 37.1, lng: -95.7 },
+    zoom: 3,
+  },
+  uk: {
+    center: { lat: 54.8, lng: -4.6 },
+    zoom: 5,
+  },
+};
+
 function initMap() {
-    var options = {
-        zoom: 4,
-        center: { lat: 53.29345, lng: -9.4764513},
+$("#hotelRadio").prop("checked" , true);
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: countries ["uk"].center,
+    zoom: countries ["uk"].zoom,
+    mapTypeControl: false,
+    panControl: false,
+    zoomControl: false,
+    streetViewControl: false,
+  });
+
+infoWindow = new google.maps.InfoWindow({
+    content: document.getElementById("info-content")
+});
+//Create the autocomplete and associate it with UI input control
+
+autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("autocomplete"),
+    {
+        types: ["(cities)"],
+        componentRestrictions: countryRestrict,
     }
-  
-  var map = new google.maps.Map(document.getElementById("map"), options);
+);
+places = new google.maps.places.PlacesService(map);
+autocomplete.addListener("place_changed" , onPlacedChanged);
+document.getElementById("foodRadio").addEventListener("change" , onPlacedChanged);
+document.getElementById("hotelRadio").addEventListener("change" , onPlacedChanged);
+document.getElementById("attractionsRadio").addEventListener("change" , onPlacedChanged);
+document.getElementById("country").addEventListener("change" , setAutocompleteCountry);
+document.getElementById("reset-button").addEventListener("change" , setAutocompleteCountry);
+}
+
+function onPlaceChanged() {
+    if($("hotelRadio").is(":checked")) {
+        var place = autocomplete.getPlace();
+        if (place.geometry) {
+            map.panTo(place.geometry.location);
+            map.setZoom(15);
+            searchHotel();
+        }  else {
+            $("#autocomplete").attr("placeholder" , "City");
+        }
+    }
+    else if ($("#foodRadio").is(":checked")) {
+        var place = autocomplete.getPlace();
+        if (place.geometry) {
+            map.panTo(place.geometry.location);
+            map.setZoom(15);
+            searchRestaurant();
+        }
+    else {
+            $("#autocomplete").attr("placeholder" , "City");
+        }
+    }
+    else if ($("#attractionsRadio").is(":checked")) {
+        var place = autocomplete.getPlace();
+        if (place.geometry) {
+            map.panTo(place.geometry.location);
+            map.setZoom(15);
+            searchAttractions();
+        }
+    else {
+        $("#autocomplete").attr("placeholder" , "City");
+    }
+}
+
+//search for hotels in selected city
+function searchHotel() {
+    var search = {
+        bounds: map.getBounds(),
+        types: ["lodging"]
+    };
+    places.nearbySearch(search, function(results, status) {
+     if (status === google.maps.places.PlacesServiceStatus.OK) {
+       clearResults();
+       clearMarkers();
+       
+    // Create a marker for each hotel found, and
+    // assign a letter of the alphabetic to each marker
+    for (var i = 0; i < results.length; i++) {
+        var markerLetter = String.fromCharCode("A" .charCodeAt(0) + (i % 26));
+        var markerIcon = MARKER_PATH + markerLetter + ".png";
+    //Marker animation to drop the icons
+    markers[i] = new google.maps.Marker({
+        position: results[i].geometry.location,
+        animation: google.maps.Animation.DROP,
+        icon: markerIcon
+    });
+    //If user clicks on marker show details in an info window
+    markers[i].placeResult = results[i];
+    google.maps.event.addListener(markers[i], "click", showInfoWindow);
+    setTimeout(dropMarker(i), i * 100);
+    addResult(results[i], i);
+    }
+}
+});
+}
+//search for restaurants in selected city
+function searchRestaurant() {
+    var search = {
+        bounds: map.getBounds(),
+        types: ["restaurants", "bar"]
+    };
+    places.nearbySearch(search, function(results, status) {
+     if (status === google.maps.places.PlacesServiceStatus.OK) {
+       clearResults();
+       clearMarkers();
+       
+    // Create a marker for each restaurant found, and
+    // assign a letter of the alphabetic to each marker
+    for (var i = 0; i < results.length; i++) {
+        var markerLetter = String.fromCharCode("A" .charCodeAt(0) + (i % 26));
+        var markerIcon = MARKER_PATH + markerLetter + ".png";
+    //Marker animation to drop the icons
+    markers[i] = new google.maps.Marker({
+        position: results[i].geometry.location,
+        animation: google.maps.Animation.DROP,
+        icon: markerIcon
+    });
+    //If user clicks on marker show details in an info window
+    markers[i].placeResult = results[i];
+    google.maps.event.addListener(markers[i], "click", showInfoWindow);
+    setTimeout(dropMarker(i), i * 100);
+    addResult(results[i], i);
+    }
+}
+});
+}
+//search for attractions in selected city
+function searchAttractions() {
+    var search = {
+        bounds: map.getBounds(),
+        types: ["museum", "art_gallery", "park"]
+    };
+    places.nearbySearch(search, function(results, status) {
+     if (status === google.maps.places.PlacesServiceStatus.OK) {
+       clearResults();
+       clearMarkers();
+       
+    // Create a marker for each hotel found, and
+    // assign a letter of the alphabetic to each marker
+    for (var i = 0; i < results.length; i++) {
+        var markerLetter = String.fromCharCode("A" .charCodeAt(0) + (i % 26));
+        var markerIcon = MARKER_PATH + markerLetter + ".png";
+    //Marker animation to drop the icons
+    markers[i] = new google.maps.Marker({
+        position: results[i].geometry.location,
+        animation: google.maps.Animation.DROP,
+        icon: markerIcon
+    });
+    //If user clicks on marker show details in an info window
+    markers[i].placeResult = results[i];
+    google.maps.event.addListener(markers[i], "click", showInfoWindow);
+    setTimeout(dropMarker(i), i * 100);
+    addResult(results[i],  i);
+    }
+}
+});
+}
+
+function clearMarkers() {
+    for (var i = 0; i< markers.length; i++) {
+        if (markers[i]) {
+            markers[i].setMap(null);
+        }
+    }
+    markers = [];
+}
 }
